90784d1e867986a5fc7fb087dc966b14f722cee7
JADEON-PC\JD <jadeon77@gmail.com>
2018-09-30 22:13:28 -0500

    9-30-22:12 Making more unit test mistakes!

--- src/app/resulttable/resulttable.component.spec.ts
+++ src/app/resulttable/resulttable.component.spec.ts
@@ -1,25 +1,19 @@
 
 import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResulttableComponent } from './resulttable.component';
-import { MatTableModule, MatSnackBar } from '@angular/material';
-import { reload } from './resulttable.component';
-
-describe ( 'reload', () => {
-  it ( 'should open the snackbar', () => {
-    reload();
-    spyOn(this.snackBar, 'open');
-  })
-});
+import { MatTableModule } from '@angular/material';
 
 describe('ResulttableComponent', () => {
   let component: ResulttableComponent;
   let fixture: ComponentFixture<ResulttableComponent>;
-  let snackBar: MatSnackBar;
+
+  component.reloadData();
+
+  expect()
 
   beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResulttableComponent ],
-      imports: [MatSnackBar]
+      declarations: [ ResulttableComponent ]
     })
     .compileComponents();
 
@@ -32,6 +26,4 @@ describe('ResulttableComponent', () => {
   it('should compile', () => {
     expect(component).toBeTruthy();
   });
-
-
 });
